697a99f1fda19544789cbcc21dc7e2921d269afd
Magleno <glen.chevalier@gmail.com>
2021-10-02 16:18:23 +0200

    Added WIFI and influx database informations

--- Main_sketch_ESP32/Main_sketch_ESP32.ino
+++ Main_sketch_ESP32/Main_sketch_ESP32.ino
@@ -1,6 +1,20 @@
 #include <InfluxDbClient.h>
 #include <EmonLib.h>
+#include <WiFiMulti.h>
+WiFiMulti wifiMulti;
+#define DEVICE "ESP32"
 
+#include <InfluxDbClient.h>
+
+// WiFi AP SSID
+#define WIFI_SSID "Bbox-D829F859"
+// WiFi password
+#define WIFI_PASSWORD "Shipman56570"
+// InfluxDB  server url. Don't use localhost, always server name or ip address.
+// E.g. http://192.168.1.48:8086 
+#define INFLUXDB_URL "http://192.168.1.9:8086"
+// InfluxDB v1 database name 
+#define INFLUXDB_DB_NAME "home"
 
 void setup() {
   // put your setup code here, to run once:
